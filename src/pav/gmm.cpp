c80182c9fc5228f17172a2096d3c6f19c635d9b6
Marc Heras Puig <marc.heras.puig@estudiantat.upc.edu>
2023-05-15 17:57:45 +0200

    codigo(pdf)

--- src/pav/gmm.cpp
+++ src/pav/gmm.cpp
@@ -112,6 +112,7 @@ namespace upc
 
 		for (n=0; n<data.nrow(); ++n) {
 			/// \TODO Compute the logprob of a single frame of the input data; you can use gmm_logprob() above.
+			lprob=lprob+gmm_logprob(data[n]);
 		}
 		return lprob/data.nrow();
 	}
@@ -203,7 +204,7 @@ namespace upc
 	int GMM::em(const fmatrix &data, unsigned int max_it, float inc_threshold, int verbose) {
 		unsigned int iteration;
 		float old_prob=-1e34, new_prob=-1e34, inc_prob=-1e34;
-
+		
 		fmatrix weights(data.nrow(), nmix);
 		for (iteration=0; iteration<max_it; ++iteration) {
 			/// \TODO
@@ -213,6 +214,12 @@ namespace upc
 			//
 			// Update old_prob, new_prob and inc_prob in order to stop the loop if logprob does not
 			// increase more than inc_threshold.
+			old_prob = new_prob;
+			new_prob = this->em_expectation(data, weights);
+			this->em_maximization(data, weights);
+			inc_prob = new_prob - old_prob;
+			if(inc_prob < inc_threshold)
+				break;
 			if (verbose & 01)
 				cout << "GMM nmix=" << nmix << "\tite=" << iteration << "\tlog(prob)=" << new_prob << "\tinc=" << inc_prob << endl;
 		}
